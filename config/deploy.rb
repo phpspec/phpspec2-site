287508081819584dcca2ac23e0b64cb43b55495f
Marcello Duarte <marcello.duarte@gmail.com>
2013-05-22 23:54:02 +0100

    Add capistrano deployment script

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "phpspec_site"
+set :repository,  "git@github.com:phpspec/phpspec2-site.git"
+set :deploy_to, "/home/deploy"
+set :current, "#{deploy_to}/current"
+set :user, "deploy"
+set :pre_build_unused_files, ['_static', '_templates', 'conf.py', 'index.html',
+  'index.txt', 'Makefile', 'README.md', 'docs', 'REVISION', '_build/public']
+set :post_build_unused_files, ['_build', '_sources', 'log', 'index.rst', 'tmp',
+  'public', 'objects.inv', 'docs']
+set :scm, :git
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+role :web, "www.phpspec.net"
+
+after "deploy", "compile_sphinx_files"
+
+task :compile_sphinx_files do
+  run "cd #{current} && make html"
+  remove_pre_build_unused_files
+  run "cp -r #{current}/_build/html/* #{current}/."
+  remove_post_build_unused_files
+end
+
+def remove_pre_build_unused_files()
+  fetch(:pre_build_unused_files).each do |path|
+    run "rm -fr #{current}/#{path}"
+  end
+end
+
+def remove_post_build_unused_files()
+  fetch(:post_build_unused_files).each do |path|
+    run "rm -fr #{current}/#{path}"
+  end
+end
